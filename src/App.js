9e3bc0de104a457bd6e66f7bd9fa54f225a35689
danny14 <danny_ruiz_1995@hotmail.com>
2023-03-22 23:50:01 -0500

    Create Navbar and Footer and Create Home.jsx 18:09

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store'
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="text-blue-700">
       App
     </div>
+    </Provider>
   );
 }
 
